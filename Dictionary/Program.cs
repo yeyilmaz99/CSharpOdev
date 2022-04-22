f5cb36d578e02f5c3a30e3e2a3dae2850acd79e7
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-04-23 02:21:10 +0300

    Dictionary

---
+++ Dictionary/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Dictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // koleksiyonun oluşturulması
+            Dictionary<int,string> myDictionary = new Dictionary<int,string>();
+            //değerlerin eklenmesi
+            myDictionary.Add(1, "Yunus");
+            myDictionary.Add(2, "Yagmur");
+            myDictionary.Add(3, "Emir");
+
+
+            foreach (KeyValuePair<int,string> item in myDictionary)
+            {
+                Console.WriteLine("{0} {1}",
+                    item.Key, item.Value);
+            }
+
+
+            // koleksiyonun oluşturulması ve aynı zamanda değerlerin eklenmesi
+            Dictionary<string,string> myDictionary2 = new Dictionary<string, string>(){ { "1-Its a", "Dog"},{ "2-Its a", "Frog"},{ "3-Its a", "Cat"} };
+
+
+            foreach (KeyValuePair<string,string> item in myDictionary2)
+            {
+                Console.WriteLine("{0} {1}", 
+                    item.Key, item.Value);
+            }
+        }
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<string> sehirler = new List<string>();
+            sehirler.Add("ankara");
+            sehirler.Add("ankara");
+            sehirler.Add("ankara");
+            sehirler.Add("ankara");
+            sehirler.Add("ankara");
+            Console.WriteLine(sehirler.Count);
+
+            List<int> plakalar = new List<int>();
+            //MyList sehirler2 = new MyList();
+
+
+            MyList<int> plakalar2 = new MyList<int>();
+            plakalar.Add(1);
+
+            MyList<string> sehirler2 = new MyList<string>();
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            sehirler2.Add("Eskişehir");
+            Console.WriteLine(sehirler2.Count);
+            
+        }
+    }
+
+    class MyList<T>
+    {
+        T[] _array;
+        T[] _tempArray;
+
+        public MyList()
+        {
+                _array = new T[0];
+        }
+         public void Add(T item)
+        {
+            _tempArray = _array;
+            _array = new T[_array.Length + 1];
+            for(int i = 0; i < _tempArray.Length; i++)
+            {
+                _array[i] = _tempArray[i];
+            }
+            _array[_array.Length-1] = item;
+        }
+
+        public int Count
+        {
+            get { return _array.Length; }
+        }
+
+    }
+}
