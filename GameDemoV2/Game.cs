56f9baecbef36d2f7b8e923bc52b90463a74b9b6
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-04-27 23:55:28 +0300

    GameDemoV2

---
+++ GameDemoV2/Game.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDemoV2
+{
+    internal class Game
+    {
+        public int GameID { get; set; }
+        public string GameName { get; set; }
+        public int UnitPrice { get; set; }
+    }
+}
