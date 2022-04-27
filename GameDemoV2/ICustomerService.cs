23990715d65748072e4fb570c5d89725ea62f3b5
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-04-27 23:58:36 +0300

    GameDemoV2

---
+++ GameDemoV2/ICustomerService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDemoV2
+{
+    internal interface ICustomerService
+    {
+        public void Add();
+        public void Update();   
+        public void Delete();
+    }
+}
---
+++ GameDemoV2/IGameService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameDemoV2
+{
+    internal interface IGameService
+    {
+        public void Add();
+        public void Delete();
+        public void Update();
+    }
+}
