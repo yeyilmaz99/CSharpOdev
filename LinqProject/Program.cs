6ea59603d49408abbf2ca73561b2d0e20378ee20
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-04-29 02:29:14 +0300

    LinqProject

---
+++ LinqProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LinqProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+
+}
