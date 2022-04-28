00f86755f29e9412a01136790ad43c6b31cd4ee7
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-04-29 02:32:26 +0300

    LinqProject

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -9,5 +9,20 @@ namespace LinqProject
             Console.WriteLine("Hello World!");
         }
     }
+    class Product
+    {
+        public int ProductId { get; set; }
+        public int CategoryId { get; set; }
+        public string ProductName { get; set; }
+        public string QuantityPerUnit { get; set; }
+        public decimal UnitPrice { get; set; }
+        public int UnitsInStock { get; set; }
 
+    }
+
+    class Category
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+    }
 }
