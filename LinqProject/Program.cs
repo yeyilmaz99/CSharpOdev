0b5bb8581bd1ae4852449fe68b0cea8a48ec04e6
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-01 13:48:34 +0300

    Linq Sorguları

--- LinqProject/Program.cs
+++ LinqProject/Program.cs
@@ -17,21 +17,95 @@ namespace LinqProject
             List<Product> products = new List<Product>()
             {
                 new Product(){ProductId = 1, CategoryId=1,ProductName="Acer Laptop",QuantityPerUnit ="32 GB Ram", UnitPrice =10000,UnitsInStock=5},
-                new Product(){ProductId = 2, CategoryId=1,ProductName="Asus Laptop",QuantityPerUnit ="16 GB Ram", UnitPrice =8000,UnitsInStock=3},
-                new Product(){ProductId = 3, CategoryId=1,ProductName="Hp Laptop",QuantityPerUnit ="8 GB Ram", UnitPrice =6000,UnitsInStock=2},
+                new Product(){ProductId = 2, CategoryId=1,ProductName="Asus Laptop",QuantityPerUnit ="16 GB Ram", UnitPrice =9000,UnitsInStock=3},
+                new Product(){ProductId = 3, CategoryId=1,ProductName="Hp Laptop",QuantityPerUnit ="8 GB Ram", UnitPrice =7000,UnitsInStock=2},
                 new Product(){ProductId = 4, CategoryId=2,ProductName="Samsung Telefon",QuantityPerUnit ="4 GB Ram", UnitPrice =5000,UnitsInStock=15},
-                new Product(){ProductId = 5, CategoryId=2,ProductName="Apple Telefon",QuantityPerUnit ="4 GB Ram", UnitPrice =8000,UnitsInStock=0}
+                new Product(){ProductId = 5, CategoryId=2,ProductName="Apple Telefon",QuantityPerUnit ="4 GB Ram", UnitPrice =6000,UnitsInStock=0}
             };
 
+            //Test(products);
 
+            //List<Product> products1 = GetProducts(products);
+
+            //List<Product> products2 = GetProductsLinq(products);
+
+            //foreach (var p in products1)
+            //{
+            //    Console.WriteLine("Algoritmik");
+            //    Console.WriteLine(p.ProductName);
+            //}
+
+            //foreach (var p in products2)
+            //{
+            //    Console.WriteLine("Linq");
+            //    Console.WriteLine(p.ProductName);
+            //}
+
+
+
+            //AnyTest(products);
+
+            //FindTest(products);
+
+
+            //FindAllTest(products);
+
+            //AscDescTest(products);
+
+            var result = from p in products
+                         where p.UnitPrice>=6000
+                         orderby p.UnitPrice descending, p.ProductName ascending
+                         select p;
+
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+
+        }
+
+        private static void AscDescTest(List<Product> products)
+        {
+            //single line query
+            var result = products.Where(p => p.ProductName.Contains("top")).OrderByDescending(p => p.UnitPrice).ThenByDescending(p => p.ProductName);
+
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void FindAllTest(List<Product> products)
+        {
+            var result = products.FindAll(p => p.ProductId > 0);
+            foreach (var product in result)
+            {
+                Console.WriteLine(product.ProductName);
+            }
+        }
+
+        private static void FindTest(List<Product> products)
+        {
+            var result = products.Find(p => p.ProductId == 3);
+            Console.WriteLine(result.ProductName);
+        }
+
+        private static void AnyTest(List<Product> products)
+        {
+            var result = products.Any(p => p.ProductName == "Acer Laptop"); // listenin içerisinde aradığımız eleman var mı yok mu bakmamıza yarar
+            Console.WriteLine(result);
+        }
+
+        private static void Test(List<Product> products)
+        {
             Console.WriteLine("Algoritmik-------------");
             foreach (var product in products)
             {
-                if (product.UnitPrice > 5000 && product.UnitsInStock>3)
+                if (product.UnitPrice > 5000 && product.UnitsInStock > 3)
                 {
                     Console.WriteLine(product.ProductName);
                 }
-                
+
             }
 
             Console.WriteLine("Linq---------------");
@@ -42,32 +116,8 @@ namespace LinqProject
             {
                 Console.WriteLine(product.ProductName);
             }
-
-
-
-
-
-            List<Product> products1 = GetProducts(products);
-
-            List<Product> products2 = GetProductsLinq(products);
-
-            foreach(var p in products1)
-            {
-                Console.WriteLine("Algoritmik");
-                Console.WriteLine(p.ProductName);
-            }
-
-            foreach (var p in products2)
-            {
-                Console.WriteLine("Linq");
-                Console.WriteLine(p.ProductName);
-            }
-
-
-
-
-
         }
+
         static List<Product> GetProducts(List<Product> products)
         {
             List<Product> filteredProduct = new List<Product>();
