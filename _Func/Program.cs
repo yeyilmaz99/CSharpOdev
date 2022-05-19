7bf69baa70b52f5669d93de47ae8fac44ef07877
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-20 00:33:58 +0300

    11.gün ödev

---
+++ _Func/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _Func
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Topla(2,3));
+
+            Func<int, int, int> add = Topla;
+
+            Console.WriteLine(add(3,5));
+        }
+
+        public static int Topla(int x,int y)
+        {
+            return x + y;
+        }
+    }
+}
