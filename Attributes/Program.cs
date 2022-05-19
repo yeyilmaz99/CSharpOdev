dd8d5ee2542af7685b7030807ce150149e7ebcb5
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-19 22:15:41 +0300

    11.gün ödev

---
+++ Attributes/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace Attributes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer = new Customer() { Id=1, LastName="Yılmaz",Age=23}; 
+            CustomerDal customerDal = new CustomerDal();
+
+            customerDal.Add(customer);  
+        }
+    }
+
+
+    [ToTable("Customers")]
+    class Customer
+    {
+        public int Id { get; set; }
+        [RequiredProperty]
+        public string FirstName { get; set; }
+        [RequiredProperty]
+        public string LastName { get; set; }
+        [RequiredProperty]
+        public int Age { get; set; }
+    }
+
+    class CustomerDal
+    {
+        public void Add(Customer customer)
+        {
+            Console.WriteLine(" {0},{1},{2},{3} added", customer.Id,customer.FirstName,customer.LastName,customer.Age);
+        }
+    }
+
+
+    class RequiredPropertyAttribute: Attribute
+    {
+
+    }
+
+    class ToTableAttribute : Attribute
+    {
+        private string _tableName;
+
+        public ToTableAttribute(string tableName)
+        {
+            tableName = _tableName;
+        }
+    }
+}
