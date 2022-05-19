bfbfde948feb4cc19c74286e04b0ab10c92026de
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-20 00:42:43 +0300

    11.gün ödev

--- _Func/Program.cs
+++ _Func/Program.cs
@@ -11,6 +11,20 @@ namespace _Func
             Func<int, int, int> add = Topla;
 
             Console.WriteLine(add(3,5));
+
+
+            Func<int> getRandomNumber = delegate ()
+            {
+                Random random = new Random();
+                return random.Next(1, 100);
+            };
+
+            Console.WriteLine(getRandomNumber());
+
+
+            Func<int> getRandomNumber2 = () => new Random().Next(1, 100);
+
+            Console.WriteLine(getRandomNumber2());
         }
 
         public static int Topla(int x,int y)
