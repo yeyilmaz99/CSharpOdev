aec9fd20faa8273a00de6c95599af40c9d913d94
yeyilmaz99 <yeyilmaz34@gmail.com>
2022-05-20 00:20:55 +0300

    11.gün ödev

--- Delegates/Program.cs
+++ Delegates/Program.cs
@@ -4,11 +4,53 @@ namespace Delegates
 {
 
     public delegate void MyDelegate();
+    public delegate void MyDelegate2(string text);
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CustomerManager customerManager = new CustomerManager();
+            //customerManager.SendMessage();
+            //customerManager.ShowAlert();
+
+            MyDelegate myDelegate = customerManager.SendMessage;
+            myDelegate += customerManager.ShowAlert;
+
+            myDelegate -= customerManager.SendMessage;
+
+            MyDelegate2 myDelegate2 = customerManager.SendMessage;
+
+            myDelegate();
+        }
+
+
+        public class CustomerManager
+        {
+            public void SendMessage()
+            {
+                Console.WriteLine("Hello!");
+            }
+
+
+            public void ShowAlert()
+            {
+                Console.WriteLine("Be Careful!");
+            }
+
+
+            public void SendMessage2(string message)
+            {
+                Console.WriteLine("Hello!");
+            }
+
+
+            public void ShowAlert2(string alert)
+            {
+                Console.WriteLine("Be Careful!");
+            }
+
+
+
         }
     }
 }
